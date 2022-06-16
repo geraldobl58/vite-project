eeab8c6eb488e4630866354c28566a5d18ec7b7f
Geraldo Luiz <geraldobl58@gmail.com>
2022-06-16 14:16:58 -0300

    :sparkles: Creating internal sidebar components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,12 +10,18 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"  />
     
       <div className={styles.profile}>
+        <img
+          className={styles.avatar} 
+          src="https://avatars.githubusercontent.com/u/22802518?v=4" 
+        />
         <strong>John Doe</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+        <PencilLine size={20} />
+
           Editar seu perfil
         </a>
       </footer>
