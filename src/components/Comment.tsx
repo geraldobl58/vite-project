1e992385049fc86cc57d52bff8087e1d5a1a5bc6
Geraldo Luiz <geraldobl58@gmail.com>
2022-06-18 15:28:51 -0300

    :sparkles: Creating avatar component

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,11 +1,16 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 
+import { Avatar } from './Avatar'
+
 import styles from './Comment.module.css'
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://avatars.githubusercontent.com/u/22802518?v=4" />
+       <Avatar 
+        hasBorder={false} 
+        image='https://avatars.githubusercontent.com/u/22802518?v=4' 
+      />
       
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
