ad651574e7b433c36c54e14051debf53ee5aa51c
Geraldo Luiz <geraldobl58@gmail.com>
2022-06-19 12:29:32 -0300

    :sparkles: Creating functionality for the post component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,37 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/22802518?v=4',
+      name: 'John Doe',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-06-19 12:08:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/6412038?v=4',
+      name: 'Jane Doe',
+      role: 'Software Engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'O melhor lorem ipsum do mundis. 👋' },
+      { type: 'paragraph', content: 'Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é.Leite de capivaris, leite de mula manquis sem cabeça.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.Casamentiss faiz malandris se pirulitá. 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-06-15 12:08:00')
+  }
+]
+
 export function App() {
   return (
     <>
@@ -15,8 +46,13 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {posts.map((item) => (
+            <Post 
+              author={item.author.name}
+              content={item.content}
+              publishedAt={item.publishedAt} 
+            />
+          ))}
         </main>
       </div>
     </>
